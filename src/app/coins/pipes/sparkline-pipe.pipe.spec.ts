0416bbaced5cceeb51f2d25b438ccd63668c479d
Nini Alania <niniallania@gmail.com>
2023-07-28 23:47:35 +0400

    add change-currency to drawer

--- src/app/coins/pipes/sparkline-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SparklinePipePipe } from './sparkline-pipe.pipe';
-
-describe('SparklinePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SparklinePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
